7a7c41aee0365a478b9f23d79c7dc546e1850f1d
Tareq Newaz Shahriar <tariq.newaz.shahriar@live.com>
2020-11-11 20:17:06 +0600

    view-component used. script injected from view-component.

---
+++ aspnet-core/src/Testing.Web.Mvc/wwwroot/view-resources/Views/Shared/Components/Printing/Default.js
@@ -0,0 +1,24 @@
+﻿(function ($) {
+    let openerButton = $('button#print-dialog-opener');
+    if (openerButton.length) {
+        openerButton.click(function () {
+            abp.ajax({
+                url: $('input#print-view-url').val(),
+                type: 'GET',
+                dataType: 'html',
+                success: function (content) {
+                    let modal = $('#PrintModal');
+                    modal.find('.modal-body').html(content);
+
+                    modal.find('button#print').off().on('click', function () {
+                        window.print();
+                        $('#PrintModal').modal('hide');
+                    });
+                },
+                error: function (e) {
+                    console.error('on print component', e);
+                }
+            });
+        });
+    }
+})(jQuery);
